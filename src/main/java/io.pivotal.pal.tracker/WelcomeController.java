28f7b36343b6c155a7145d5b51e1594176b05f95
n0306672 <Saravanan-14.S-14@Cognizant.com>
2020-05-28 16:28:57 -0400

    Assignment 2

--- src/main/java/io.pivotal.pal.tracker/WelcomeController.java
+++ src/main/java/io.pivotal.pal.tracker/WelcomeController.java
@@ -2,9 +2,17 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class WelcomeController {
+
+    private final String displayMessage;
+
+    public WelcomeController( @Value("${welcome.message}") String displayMessage)
+    {
+        this.displayMessage = displayMessage;
+    }
 
     @GetMapping("/")
     public String sayHello()
